3f8976ec762fb8ebf49f2d1af6db7769070d4003
Mikhail Tyuzin <mikhail_tyuzin@epam.com>
2018-06-01 00:22:12 +0300

    Added test for autodiscovery publisher

--- test/test_zabbix_autodiscovery_publisher.js
+++ test/test_zabbix_autodiscovery_publisher.js
@@ -3,21 +3,23 @@ const events = require('events');
 const logger = require('util');
 const sinon = require('sinon');
 const zabbix = require('../lib/zabbix');
+const publisherFactory = require('../lib/zabbix-autodiscovery-publisher');
 
 const config = {
   flushInterval: 1,
   zabbixDiscoveryKey: '#AUTO_DISCOVERY_METRIC#',
-  zabbixFilters: ['../lib/zabbix-autodiscovery-publisher'],
+  zabbixPublisher: '../lib/zabbix-autodiscovery-publisher',
+  zabbixMaxDiscoveryBatchSize: 4,
+  zabbixReportPublishStats: true,
 };
 
-describe('filtering works', () => {
-  it('zabbix backend can load filters', () => {
+describe('zabbix backend works', () => {
+  it('zabbix backend can load publisher', () => {
     const emitter = new events.EventEmitter();
     zabbix.init(0, config, emitter, logger);
-    assert.equal(zabbix.allFilters().length, 1);
   });
 
-  it('filter emits discovery items', () => {
+  it('publisher emits discovery items', () => {
     logger.log = sinon.spy();
     const emitter = new events.EventEmitter();
     zabbix.init(0, config, emitter, logger);
@@ -31,7 +33,6 @@ describe('filtering works', () => {
       pctThreshold: [],
     });
     sinon.assert.called(logger.log);
-    assert.equal(zabbix.stats.flush_length, 6);
   });
 
   it('filter does not emit discovery item second time', () => {
@@ -53,3 +54,93 @@ describe('filtering works', () => {
     assert.equal(zabbix.stats.flush_length, 4);
   });
 });
+
+describe('zabbix autodiscovery publisher works', () => {
+  const createBatch = (len) => {
+    const batch = [];
+    for (let i = 0; i < len; i += 1) {
+      batch.push({
+        host: 'host',
+        key: `key${i + 1}`,
+        value: i + 1,
+      });
+    }
+    return batch;
+  };
+
+  const createBatchSender = () => {
+    const instance = {
+      items: [],
+      addFailed: 0,
+    };
+    instance.publishBatch = (batch, callback) => {
+      let count = 0;
+      if (batch) {
+        batch.forEach(item => {
+          instance.items.push(item);
+          count += 1;
+        });
+      }
+      callback({
+        processed: count - instance.addFailed,
+        failed: instance.addFailed,
+        total: count,
+        secondsSpend: 0.1,
+      });
+    };
+
+    instance.complete = (callback) => {
+      if (callback) {
+        callback({
+          processed: instance.items.length - instance.addFailed,
+          failed: instance.addFailed,
+          total: instance.items.length,
+          secondsSpend: 0.1,
+        });
+      }
+    };
+    return instance;
+  }
+
+  it('publisher can init', () => {
+    logger.log = sinon.spy();
+    const publisher = publisherFactory(config, logger);
+  });
+
+  it('publisher emits discovery item for short batch', () => {
+    logger.log = sinon.spy();
+    const items = createBatch(2);
+    const batchSender = createBatchSender();
+    const publisher = publisherFactory(config, logger);
+    publisher(items, batchSender);
+    assert.equal(batchSender.items.length, 3);
+  });
+
+  it('publisher emits discovery items for long batch', () => {
+    logger.log = sinon.spy();
+    const items = createBatch(6);
+    const batchSender = createBatchSender();
+    const publisher = publisherFactory(config, logger);
+    publisher(items, batchSender);
+    assert.equal(batchSender.items.length, 8);
+  });
+
+  it('publisher emits publish stats items', () => {
+    logger.log = sinon.spy();
+    const items1 = createBatch(2);
+    const items2 = items1.slice();
+    const items3 = items1.slice();
+    const batchSender1 = createBatchSender();
+    const batchSender2 = createBatchSender();
+    const batchSender3 = createBatchSender();
+    const publisher = publisherFactory(config, logger);
+    publisher(items1, batchSender1);
+    publisher(items2, batchSender2);
+    publisher(items3, batchSender3);
+    assert.equal(batchSender1.items.length, 3, 'first time only items + discovery');
+    assert.equal(batchSender2.items.length, 7, 'second time items, discovery and publish stats');
+    assert.equal(batchSender3.items.length, 6, 'third time items and publish stats');
+  });
+});
+
+
